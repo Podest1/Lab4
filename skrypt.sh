f225a836a3b973fac020a93d5184452efadcb6d3
Dominik Podhorodecki <podhorodeckidominik@gmail.com>
2025-04-17 10:18:21 +0200

    Dodano flagę --logs

--- skrypt.sh
+++ skrypt.sh
@@ -2,3 +2,11 @@
 if [[ $1 == "--date" || $1 == "-d" ]]; then
   date
 fi
+if [[ $1 == "--logs" || $1 == "-l" ]]; then
+  count=${2:-100}
+  for i in $(seq 1 $count); do
+    echo "Nazwa pliku: log$i.txt" > log$i.txt
+    echo "Skrypt: skrypt.sh" >> log$i.txt
+    date >> log$i.txt
+  done
+fi
