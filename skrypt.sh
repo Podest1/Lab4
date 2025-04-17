adfb02feddf46abbd2019c48b74da99f4cdc24d5
Dominik Podhorodecki <podhorodeckidominik@gmail.com>
2025-04-17 10:19:44 +0200

    Dodano flagę --error

--- skrypt.sh
+++ skrypt.sh
@@ -20,3 +20,12 @@ if [[ $1 == "--init" ]]; then
   git clone https://github.com/twoja_nazwa/Lab4-Git-Tools.git $PWD/repo
   export PATH=$PATH:$PWD/repo
 fi
+if [[ $1 == "--error" || $1 == "-e" ]]; then
+  count=${2:-100}
+  for i in $(seq 1 $count); do
+    mkdir -p error$i
+    echo "Nazwa pliku: error$i.txt" > error$i/error$i.txt
+    echo "Skrypt: skrypt.sh" >> error$i/error$i.txt
+    date >> error$i/error$i.txt
+  done
+fi
